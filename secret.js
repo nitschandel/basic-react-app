e531d0db11765414d99874c51cbc488992944348
Nitesh Chandel <nitesh.chandel61@gmail.com>
2023-11-27 17:09:49 +0530

    fourth test

--- secret.js
+++ secret.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const sql = require('mssql');
+const { Pool } = require('pg');
 
 const app = express();
+
+const pool = new Pool({
+  user: 'your_username',
+  host: 'your_host',
+  database: 'your_database',
+  password: 'your_password',
+  port: 5432,
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -12,14 +20,17 @@ app.post('/login', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  const query = `SELECT * FROM Users WHERE username = '${username}' AND password = '${password}'`;
+  const query = {
+    text: 'SELECT * FROM users WHERE username = $1 AND password = $2',
+    values: [username, password],
+  };
 
-  sql.query(query, (err, result) => {
+  pool.query(query, (err, result) => {
     if (err) {
       return res.status(500).send('Internal Server Error');
     }
 
-    if (result.recordset.length > 0) {
+    if (result.rows.length > 0) {
       return res.send('Login Successful');
     } else {
       return res.status(401).send('Login Failed');
